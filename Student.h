34c3e911c021e824721c102a3e959e085505800d
janaelmenshawi <jana.elmenshawi@student.giu-uni.de>
2025-11-11 09:51:34 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -6,7 +6,23 @@
 #using namespace std;
 //#write student class here
 
+class Student : public person{
+private:
+    int yearLevel;
+    string major;
+public:
+    Student() {
+        this->yearLevel = 0;
+        this->major = "";
+    };
+    Student(string name, int id, string major, int yearLevel):person(name,id) {
+        this->major = major;
+        this->yearLevel = yearLevel;
+    }
+    ~Student();
+    void display();
 
+};
 
 
 
