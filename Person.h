ca1d86b68e703f8ee957b331bcc24ea1129dc892
janaelmenshawi <jana.elmenshawi@student.giu-uni.de>
2025-11-11 09:52:08 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,7 +5,22 @@
 using namespace std;
 
 //#write Person class here
-
+class person {
+private:
+    string name;
+    int id;
+public:
+    person(string name, int id) {
+        this->name = name;
+        this->id = id;
+    } ;
+    person() {
+        this->name = "";
+        this->id = 0;
+    }
+    ~person() ;
+    void display();
+};
 
 
 
