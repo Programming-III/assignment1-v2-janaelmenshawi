1f2f803e5568c5aae8a4b9a3dc8d20d93692ebc7
janaelmenshawi <jana.elmenshawi@student.giu-uni.de>
2025-11-11 09:53:18 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,41 +8,55 @@
 
 using namespace std;
 
-// ==================== Person Class Implementation =========================
+void person :: display() {
+    cout<<"Name: "<<name<<endl;
+    cout<<"ID: "<<id<<endl;
+}
+void student :: display() {
+    cout<<"Student info: "<<endl;
+    person :: display();
+    cout<<"Year: "<<yearLevel<<endl;
+    cout<<"major: "<<major<<endl;
+
+}
+void instructor :: display() {
+    cout<<"Instructor info: "<<endl;
+    person :: display();
+    cout<<"Department: "<<department<<endl;
 
 
+}
+void :: course addstudent(const Student& s) {
+    if (currentStudents<maxStudents) {
+        Student *temp=new Student[currentStudents+1];
+        for (int i=0;i<currentStudents;i++) {
+            temp[i]=students[i];
+        }
+        temp[currentStudents]=s;
+        delete [] students;
+        students=temp;
+    }
+}
 
+void :: course displayCourseInfo() {
+   cout<<"Course info: "<<endl;
+    cout<<"max num: "<<maxStudents<<endl;
+    cout<<"currently enrolled : "<<currentStudents<<endl;
+    for (int i=0;i<currentStudents;i++) {
+       students[i].display();
+    }
+}
 
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
 int main() {
-   
-    
+    student x("jana",1,"s",2);
+    instructor y("hi",12,"j",12);
+    course z("si","uds",1);
+    z.addstudent(x);
+    z.displayCourseInfo();
+    x.display();
+    y.display();
+
+
     return 0;
+    
 }
