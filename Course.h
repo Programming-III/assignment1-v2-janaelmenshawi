0fea2cfd257683e6dc9ab4cf4ecf62def091e221
janaelmenshawi <jana.elmenshawi@student.giu-uni.de>
2025-11-11 09:48:53 +0200

    Implement course class with student management
    Added a course class with constructors and destructor.

--- Course.h
+++ Course.h
@@ -6,7 +6,35 @@
 using namespace std;
 //#write your code here
 
+class course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    course() {
+        this->courseCode = "";
+        this->courseName = "";
+        this->maxStudents = 10;
+        this->students = nullptr;
+        this->currentStudents = 0;
+    }
+    course( string courseCode, string courseName, int maxStudents) {
+        this->courseCode = courseCode;
+        this->courseName = courseName;
+        this->maxStudents = maxStudents;
+        this->students = nullptr;
+        this->currentStudents = 0;
+    }
+    ~course() {
+        delete [] students;
+    }
+    void addstudent(const Student& s);
+    void displayCourseInfo();
 
+};
 
 
 
