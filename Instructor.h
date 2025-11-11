a2d3cd2bfb04f8be25e1479e86955d53d6e309cc
janaelmenshawi <jana.elmenshawi@student.giu-uni.de>
2025-11-11 09:50:49 +0200

    Implement instructor class inheriting from person

--- Instructor.h
+++ Instructor.h
@@ -4,7 +4,24 @@
 #include "Person.h"
 using namespace std;
 //#write Instructor class here
+class instructor : public person {
+private:
+    string department;
+    int experienceYears;
+public:
+    instructor() {
+        this->department = "";
+        this->experienceYears = 0;
 
+    }
+    instructor(string department, int experienceYears, string name, int id):person(name,id) {
+        this->department = department;
+        this->experienceYears = experienceYears;
+
+    }
+    ~instructor();
+    void display();
+};
 
 
 
